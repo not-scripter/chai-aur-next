fe8f651b24f7c7d0325d7116f6e01e0ab51c2731
MRINMOY MONDAL <mm2922894@gmail.com>
2024-05-27 00:40:46 +0530

    completed navbar and messagecard ib mystery message

--- nextjs-mystery-message/src/app/layout.tsx
+++ nextjs-mystery-message/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
+import NavBar from "@/components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <AuthProvider>
         <body className={inter.className}>
+          <NavBar />
           {children}
           <Toaster />
           <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
--- nextjs-mystery-message/src/components/MessageCard.tsx
+++ nextjs-mystery-message/src/components/MessageCard.tsx
@@ -1,3 +1,80 @@
-export default function MessageCard() {
-  return <div>MessageCard</div>;
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
+import { Message } from "@/model/User";
+import { useToast } from "./ui/use-toast";
+import axios from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+
+type messageCardProps = {
+  message: Message;
+  onMessageDelete: (messageId: string) => void;
+};
+
+export default function MessageCard({
+  message,
+  onMessageDelete,
+}: messageCardProps) {
+  const { toast } = useToast();
+  const handleDeleteConfirm = async () => {
+    const response = await axios.delete<ApiResponse>(
+      `/api/delete-nessage/${message._id}`,
+    );
+    toast({
+      title: response.data.message,
+    });
+    onMessageDelete(message._id);
+  };
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="destructive">
+            <X className="w-5 h-5" />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={handleDeleteConfirm}>
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </Card>
+  );
 }
