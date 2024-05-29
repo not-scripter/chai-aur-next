486290eb1991dab608d9a0352c9922e2c987dad8
MRINMOY MONDAL <mm2922894@gmail.com>
2024-05-29 21:45:38 +0530

    added hero and featured section in music academy

--- nextjs-music-academy/src/app/page.tsx
+++ nextjs-music-academy/src/app/page.tsx
@@ -1,9 +1,12 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="min-h-screen antialiased bg-black/[96] bg-grid-white/[0.2]">
-      <h1>NextJS Music Academy</h1>
+      <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
--- nextjs-music-academy/tailwind.config.ts
+++ nextjs-music-academy/tailwind.config.ts
@@ -1,5 +1,11 @@
 import type { Config } from "tailwindcss";
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,8 +20,35 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
 export default config;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
