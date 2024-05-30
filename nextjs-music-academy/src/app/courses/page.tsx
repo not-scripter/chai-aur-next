9ed95ce4b9d8dd0830105883eaf25a423bac8d04
MRINMOY MONDAL <mm2922894@gmail.com>
2024-05-30 23:59:11 +0530

    completed music academy project - pending assignment - and one major bug

---
+++ nextjs-music-academy/src/app/courses/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import courseData from "@/data/music_cources.json";
+import Image from "next/image";
+
+interface Course {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
+  image: string;
+}
+
+export default function page() {
+  return (
+    <div className="min-h-screen bg-black py-12 pt-36">
+      <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">
+        All Courses {courseData.courses.length}
+      </h1>
+      <div className="flex flex-wrap justify-center">
+        {courseData.courses.map((course: Course) => (
+          <CardContainer className="inter-var">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white"
+              >
+                {course.title}
+              </CardItem>
+              <CardItem
+                as="p"
+                translateZ="60"
+                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              >
+                {course.description}
+              </CardItem>
+              <CardItem translateZ="100" className="w-full mt-4">
+                <Image
+                  src={course.image}
+                  height="1000"
+                  width="1000"
+                  className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                  alt={course.title}
+                />
+              </CardItem>
+            </CardBody>
+          </CardContainer>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ nextjs-music-academy/src/components/Footer.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import Link from "next/link";
+
+interface quickLink {
+  title: string;
+  slug: string;
+}
+interface followUs {
+  name: string;
+  slug: string;
+}
+
+const footerItems = {
+  quickLinks: [
+    {
+      title: "Home",
+      slug: "/",
+    },
+    {
+      title: "About",
+      slug: "/about",
+    },
+    {
+      title: "Courses",
+      slug: "/courses",
+    },
+    {
+      title: "Contact Us",
+      slug: "/contact-us",
+    },
+  ],
+  followUs: [
+    {
+      name: "Facebook",
+      slug: "/",
+    },
+    {
+      name: "X",
+      slug: "/",
+    },
+    {
+      name: "Instagram",
+      slug: "/",
+    },
+    {
+      name: "LinkedIn",
+      slug: "/",
+    },
+  ],
+};
+export default function Footer() {
+  return (
+    <footer className="bg-black text-gray-400 py-12">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">About Us</h2>
+          <p className="mb-4">
+            Music School is a premier institution dedicated to teaching the art
+            and science of music. We nurture talent from the ground up,
+            fostering a vibrant community of musicians.
+          </p>
+        </div>
+        <div>
+          <h2>Quick Links</h2>
+          {footerItems.quickLinks.map((item: quickLink) => (
+            <Link
+              href={item.slug}
+              className="hover:text-white transition-colors duration-300"
+            >
+              {item.title}
+            </Link>
+          ))}
+        </div>
+        <div>
+          <h2>Follow Us</h2>
+          {footerItems.followUs.map((item: followUs) => (
+            <Link
+              href={item.slug}
+              className="hover:text-white transition-colors duration-300"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
+          <p>New Delhi, India</p>
+          <p>Delhi 10001</p>
+          <p>Email: info@musicschool.com</p>
+          <p>Phone: (123) 456-7890</p>
+        </div>
+      </div>
+      <p className="text-center text-xs pt-8">
+        © 2024 Music School. All rights reserved.
+      </p>
+    </footer>
+  );
+}
